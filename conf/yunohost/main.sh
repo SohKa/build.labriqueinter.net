751ff0d171d9cba94e16d4eedc66a197a3d1fe4d
Emile <emile@bleuchtang.fr>
2017-09-04 02:14:40 +0200

    hack to install mariadb-server

--- conf/yunohost/main.sh
+++ conf/yunohost/main.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-chroot_deb $DEBOOTSTRAP_DIR "mkdir -p /run/systemd/system/"
+if [ ${CROSS} ] ; then
+  # Warning! Crappy hack; install mysql-server before yunohost to prevent falure durring yunohost install
 
-# Hack; install mariadb-server before yunohost to prevent falure durring yunohost install 
-chroot_deb $DEBOOTSTRAP_DIR "$APT mariadb-server"
+  chroot_deb $DEBOOTSTRAP_DIR "debconf-set-selections << EOF
+mysql-server-5.5 mysql-server/root_password password yunohost
+mysql-server-5.5 mysql-server/root_password_again password yunohost
+EOF"
+
+  chroot_deb $DEBOOTSTRAP_DIR "$APT mysql-server"
+fi
 
 chroot_deb $DEBOOTSTRAP_DIR "$APT git"
 chroot_deb $DEBOOTSTRAP_DIR "git clone https://github.com/YunoHost/install_script /tmp/install_script"
 chroot_deb $DEBOOTSTRAP_DIR "cd /tmp/install_script && ./install_yunohost -a -d ${INSTALL_YUNOHOST_DIST}"
-
-chroot_deb $DEBOOTSTRAP_DIR "rmdir /run/systemd/system/ /run/systemd/ 2> /dev/null || true"
-
