9d12685d4bc4107a87f5f16a6dec0a788398901f
Emile <emile@bleuchtang.fr>
2017-09-04 02:14:13 +0200

    do not start daemon when installing debian packages

--- conf/debootstrap/main.sh
+++ conf/debootstrap/main.sh
@@ -24,8 +24,33 @@ fi
 
 echo '/dev/mmcblk0p1 / ext4 rw,relatime 0 0' > $DEBOOTSTRAP_DIR/etc/mtab
 
+# Set hostname
+echo $DEB_HOSTNAME > $DEBOOTSTRAP_DIR/etc/hostname
+sed -i "1i127.0.1.1\t${DEB_HOSTNAME}" $DEBOOTSTRAP_DIR/etc/hosts
+
 # Generate locales
 chroot_deb $DEBOOTSTRAP_DIR "$APT locales"
 sed -i "s/^# fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/" $DEBOOTSTRAP_DIR/etc/locale.gen
 sed -i "s/^# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/" $DEBOOTSTRAP_DIR/etc/locale.gen
 chroot_deb $DEBOOTSTRAP_DIR "locale-gen en_US.UTF-8"
+
+#chroot_deb $DEBOOTSTRAP_DIR "/usr/sbin/rsyslogd"
+
+#chroot_deb $DEBOOTSTRAP_DIR "mount -o remount,rw -t sysfs sysfs /sys"
+
+if [ ${CROSS} ] ; then
+  # https://anonscm.debian.org/cgit/d-i/debootstrap.git/tree/scripts/sid?id=c4ad96b3972426df3d261d33a5025335fef86f79#n159
+  chroot_deb $DEBOOTSTRAP_DIR "cat > /usr/sbin/policy-rc.d << EOF
+#!/bin/sh
+echo \"All runlevel operations denied by policy\" >&2
+exit 101
+EOF"
+  chroot_deb $DEBOOTSTRAP_DIR "chmod +x /usr/sbin/policy-rc.d"
+  chroot_deb $DEBOOTSTRAP_DIR "mv /sbin/start-stop-daemon /sbin/start-stop-daemon.REAL"
+  chroot_deb $DEBOOTSTRAP_DIR "cat > /sbin/start-stop-daemon << EOF
+#!/bin/sh
+echo \"Warning: Fake start-stop-daemon called, doing nothing\"
+exit 0
+EOF"
+  chroot_deb $DEBOOTSTRAP_DIR "chmod +x /sbin/start-stop-daemon"
+fi
